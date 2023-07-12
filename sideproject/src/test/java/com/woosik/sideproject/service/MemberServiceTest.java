22476f09c0b3ac9e95f7f5cecd89b1e6fdc031b6
namusik <79183229+namusik@users.noreply.github.com>
2023-07-12 17:44:22 +0900

    application 프로필에 따라 분리.
    테스트에는 h2 embedded 사용.
    로컬 서버는 h2 server  사용.

--- sideproject/src/test/java/com/woosik/sideproject/service/MemberServiceTest.java
+++ sideproject/src/test/java/com/woosik/sideproject/service/MemberServiceTest.java
@@ -6,19 +6,21 @@ import com.woosik.sideproject.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
+@ActiveProfiles("test")
 class MemberServiceTest {
 
     @Autowired
     MemberService memberService;
     @Autowired
     MemberRepository memberRepository;
+
     @Test
-    @Transactional
     void signUp() {
         MemberJoinDto memberJoinDto = new MemberJoinDto("test", "test@naver.com", "abc123");
         Member savedMember = memberService.signUp(memberJoinDto);
